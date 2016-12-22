b43859332ba9f76432b44383d279c3f7e6576b37
Kevin Deutscher <kevin.deutscher@americastestkitchen.com>
2016-12-22 12:09:43 -0500

    injections

---
+++ injections/injections.go
@@ -0,0 +1,50 @@
+package main
+
+
+// text/template doesn't do any escaping
+// html/template is a wrapper of text/template with all same functionality
+// allows for escaping and other features
+import (
+  "net/http"
+  "text/template"
+)
+
+// second Message works with <body><script>{{.}}</script></body> and "text/template"
+// third Message works with <body><p>{{.}}</p></body> and "text/template"
+
+// var Message string = "more beer, please"
+// var Message string = "alert('alert: you have been pwned')"
+var Message string = "<script>alert('script alert')</script>"
+
+func main() {
+  http.HandleFunc("/", myHandlerFunc)
+  http.ListenAndServe(":8080", nil)
+}
+
+func myHandlerFunc(w http.ResponseWriter, req *http.Request) {
+  w.Header().Add("Content-Type", "text/html")
+  tmpl, err := template.New("anyNameForTemplate").Parse(doc)
+  if err == nil {
+    tmpl.Execute(w, Message)
+  }
+}
+
+const doc = `
+<!DOCTYPE html>
+<html>
+<head lang="en">
+  <meta charset="UTF-8">
+  <title>Injection Safe</title>
+</head>
+<body>
+  <p>{{.}}</p>
+</body>
+</html>
+`
+
+
+
+
+
+
+//
