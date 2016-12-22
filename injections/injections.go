fc9b6d22fe4b131ea945472e18fa36ed752283fd
Kevin Deutscher <kevin.deutscher@americastestkitchen.com>
2016-12-22 12:16:29 -0500

    injections works better

--- injections/injections.go
+++ injections/injections.go
@@ -4,6 +4,7 @@ package main
 // text/template doesn't do any escaping
 // html/template is a wrapper of text/template with all same functionality
 // allows for escaping and other features
+// html/template is therefore safer and won't run alert script in this example
 import (
   "net/http"
   "text/template"
